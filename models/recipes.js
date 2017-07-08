d704da3a503c0e957fe95aa08cd5a5bb8d77b3b8
Randy <randylam87@gmail.com>
2017-07-07 19:33:21 -0700

    corrected server crashing, still working on foreign keys

--- models/recipes.js
+++ models/recipes.js
@@ -1,28 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  const Recipes = sequelize.define("Post", {
-    recipe: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
+  const Recipes = sequelize.define("recipes", {
+      recipe: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      instructions: {
+        type: DataTypes.TEXT,
+        allowNull: false,
         len: [1]
       }
-    },
-    instructions: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
-    }
-  });
-
-  Recipes.associate = (models) => {
-    Recipes.belongsTo(models.User, {
-        foreignKey: { allowNull: false }
-      }),
-      Recipes.hasMany(models.Ingredients, {
-        through: 'Measurements',
-        foreignKey: { allowNull: false }
-      });
-  };
-  
+    }, {
+      classMethods: {
+        associate: (models) => {
+          Recipes.belongsTo(models.User, {
+              foreignKey: {
+                allowNull: false
+              }
+            }),
+            Recipes.hasMany(models.Ingredients, {
+              through: 'Measurements',
+              foreignKey: {
+                allowNull: false
+              }
+            });
+        }
+      }
+    });
   return Recipes;
 };
